5f8e92192438eaa0ef2a7e272bc5041492d65e8a
Filipe Guizzo Soares <filipe.222@alunosatc.edu.br>
2026-05-11 21:06:02 -0300

    adicionando header e mais algumas mudanças descritas nos arquivos de commits

--- NexStock/App.js
+++ NexStock/App.js
@@ -1,10 +1,27 @@
-import { NavigationContainer } from "@react-navigation/native"
-import AppNavigator from "./navigation/AppNavigator"
-export default function App() {
-  return (
-    <NavigationContainer>
-      <AppNavigator/>
-    </NavigationContainer>
-  );
-}
+import { NavigationContainer } from "@react-navigation/native";
+import AppNavigator from "./navigation/AppNavigator";
+import { useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_600SemiBold } from "@expo-google-fonts/poppins";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+    const [fontsLoaded] = useFonts({
+        Poppins_400Regular,
+        Poppins_500Medium,
+        Poppins_600SemiBold,
+    });
+
+    useEffect(() => {
+        if (fontsLoaded) SplashScreen.hideAsync();
+    }, [fontsLoaded]);
+
+    if (!fontsLoaded) return null;
+
+    return (
+        <NavigationContainer>
+            <AppNavigator />
+        </NavigationContainer>
+    );
+};
